d7082e6a2c43aebaa4811ef28b10c366a69297d1
Stephanie Legarra <stephanielegarra@gmail.com>
2023-04-04 15:12:25 -0300

    badge

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import Badge from "@mui/material/Badge";
 
 const CartWidget = () => {
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        gap: "10px",
+        paddingRight: "1rem",
+      }}
+    >
       <h3> Ver carrito </h3>
-      <ShoppingCartIcon />
+      <Badge badgeContent={5} color="warning">
+        <ShoppingCartIcon />
+      </Badge>
     </div>
   );
 };
