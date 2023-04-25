cc6b9b4ed1a31e9170e3ccb19f7775758e5ed1ef
Stephanie Legarra <stephanielegarra@gmail.com>
2023-04-24 22:40:43 -0300

    estilos

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,16 @@
 import styles from "./Home.module.css";
 
+import CreditCardIcon from "@mui/icons-material/CreditCard";
+
 const Home = ({ saludo }) => {
   return (
-    <div className={styles.containerHome}>
-      <h1>{saludo}</h1>
+    <div>
+      <h2 className={styles.containerSaludo}>{saludo}</h2>
+      <div>
+        <h3 className={styles.containerPromo}>
+          Hasta 6 cuotas sin interés <CreditCardIcon />
+        </h3>
+      </div>
     </div>
   );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,23 +10,21 @@ const Navbar = () => {
           <img
             src="https://res.cloudinary.com/dpmexolyn/image/upload/v1681162456/322253871_751953889768698_7633417918375028965_n_irjuzo.jpg"
             alt="Es es el logo de la empresa"
-            className={styles.img}
           />
         </Link>
-        <ul
-          style={{
-            display: "flex",
-            gap: "40px",
-            listStyle: "none",
-            letterSpacing: "3px",
-            alignItems: "center",
-            fontWeight: "bold",
-          }}
-        >
-          <Link to="/">Todos</Link>
-          <Link to="/category/remeras">Remeras</Link>
-          <Link to="/category/buzos">Buzos</Link>
-          <Link to="/category/pantalones">Pantalones</Link>
+        <ul>
+          <Link className={styles.navegablesNavbar} to="/">
+            Todos
+          </Link>
+          <Link className={styles.navegablesNavbar} to="/category/remeras">
+            Remeras
+          </Link>
+          <Link className={styles.navegablesNavbar} to="/category/buzos">
+            Buzos
+          </Link>
+          <Link className={styles.navegablesNavbar} to="/category/pantalones">
+            Pantalones
+          </Link>
         </ul>
         <CartWidget />
       </div>
